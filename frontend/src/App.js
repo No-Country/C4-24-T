901ca23904027e6cc33b7c82ea737186c02909ad
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-04-07 18:44:14 -0300

    formularios optimizados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route,  Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import LoginClientePage from './Page/LoginClientePage';
 import RegistroClientesPage from './Page/RegistroClientesPage';
@@ -14,18 +14,21 @@ import RegistroProfesionalpage from './Page/RegistroProfesionalpage';
 function App() {
   return (
     <BrowserRouter>
-    <Navbarview/>
-      
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/logincliente' element={<LoginClientePage/>}/>
-        <Route path='/registrocliente' element={<RegistroClientesPage/>}/>
-        <Route path='/CategoriaRegistro' element={<CategoriasRegistro/>}/>
-        <Route path='/registroprofesional' element={<RegistroProfesionalpage/>}/>
-      
-    </Routes>
-  </BrowserRouter>
+      <Navbarview />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/logincliente' element={<LoginClientePage />} />
+        <Route path='/registrocliente' element={<RegistroClientesPage />} />
+        <Route path='/CategoriaRegistro' element={<CategoriasRegistro />} />
+        <Route path='/registroprofesional' element={<RegistroProfesionalpage />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
